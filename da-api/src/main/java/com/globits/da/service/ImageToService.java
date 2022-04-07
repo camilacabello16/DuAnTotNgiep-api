ec080507a411cead17d6ccf03bacee5a816baa99
manhcuong197 <manhcuong20009a@gmail.com>
2022-04-07 22:15:27 +0700

    Update ImageToService.java

--- da-api/src/main/java/com/globits/da/service/ImageToService.java
+++ da-api/src/main/java/com/globits/da/service/ImageToService.java
@@ -2,7 +2,6 @@ package com.globits.da.service;
 
 import com.globits.core.dto.FileDescriptionDto;
 import com.globits.core.service.GenericService;
-import com.globits.server.domain.Agency;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
