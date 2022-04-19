b5b444a397782d2c8e711906a3d5002079cbae32
manhcuong197 <manhcuong20009a@gmail.com>
2022-04-19 20:42:57 +0700

    fix

--- da-api/src/main/java/com/globits/da/rest/RestRegisterAccountController.java
+++ da-api/src/main/java/com/globits/da/rest/RestRegisterAccountController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.globits.security.dto.UserDto;
 import com.globits.security.service.UserService;
@@ -32,8 +33,8 @@ public class RestRegisterAccountController {
 		}
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
-	@RequestMapping(value ="/email/{email}",  method = RequestMethod.GET)
-	public ResponseEntity<Boolean> findUserByEmail(@PathVariable(name = "email") String email) {
+	@RequestMapping(value ="/email",  method = RequestMethod.POST)
+	public ResponseEntity<Boolean> findUserByEmail(@RequestParam(name = "email") String email) {
 		Boolean result = false;
 		UserDto userDto = userService.findByEmail(email);
 		if(userDto!=null) {
